c720ef27d6aa5f213c2afb00dbda37afc0cfd425
lucarab <luca_tom.rab@icloud.com>
2025-10-31 10:06:44 +0100

    Added home and about route

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { 
+  matCheckCircle, 
+  matMenu, 
+  matClose 
+} from '@ng-icons/material-icons/baseline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf, NgIcon],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [
+    provideIcons({ 
+      matCheckCircle, 
+      matMenu, 
+      matClose 
+    })
+  ]
 })
 export class App {
   protected readonly title = signal('frontend');
+  protected readonly isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
 }
